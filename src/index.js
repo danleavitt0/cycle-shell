60e2865544d3372592155eaab5845b7b87c3a951
Daniel <danleavitt0@gmail.com>
2016-03-30 19:03:57 -0700

    update middleware

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import flo from 'redux-flo'
 import handleSubmit from './middleware/handleSubmit'
 import {out} from './actions'
 import isArray from '@f/is-array'
+import logger from 'redux-logger'
 
 const defaultView = output => {
   if (typeof (output) !== 'object' || output.props) {
@@ -36,7 +37,7 @@ const cycleShell = (userUpdate, opts = {}) => {
   const {subscribe, render} = vdux({
     reducer,
     initialState: initState,
-    middleware: [flo(), handleSubmit(userUpdate), ...middleware]
+    middleware: [logger(), flo(), handleSubmit(userUpdate), ...middleware]
   })
   ready(() => {
     subscribe(state => {
--- src/middleware/handleSubmit.js
+++ src/middleware/handleSubmit.js
@@ -1,6 +1,6 @@
 import isGenerator from '@f/is-generator'
 import {logProcess, initProcess, SUBMIT, OUT} from '../actions'
-import {map} from 'yoco'
+import {composable} from 'yoco'
 
 var processCount = 0
 const handleSubmit = userUpdate => ({dispatch}) => next => action => {
@@ -8,14 +8,15 @@ const handleSubmit = userUpdate => ({dispatch}) => next => action => {
     var processId = processCount++
     dispatch(initProcess(processId, action.payload.join(' ')))
     if (isGenerator(userUpdate)) {
-      const mw = action => {
+      const mw = ({dispatch}) => next => action => {
         if (action.type === OUT) {
           return dispatch(logProcess(processId, action.payload))
-        } else {
-          return dispatch(action)
         }
+        return next(action)
       }
-      return map(mw)(userUpdate(...action.payload))
+      console.log('userUpdate', userUpdate)
+      var gen = composable([mw])(userUpdate)
+      return dispatch(gen(...action.payload))
     } else {
       return dispatch(logProcess(processId, userUpdate(...action.payload)))
     }
